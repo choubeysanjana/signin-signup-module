409f618382210dc948f2986837950a8e4b79db86
choubeysanjana <choubey.sanjana26@gmail.com>
2023-07-01 11:24:33 +0530

    signin/signup module

---
+++ server.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const app = express();
+app.use(express.json());
+
+// Connect to MongoDB (replace 'your_mongodb_url' with your actual MongoDB connection URL)
+mongoose.connect('mongodb://localhost:27017', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Failed to connect to MongoDB:', err));
+
+// Define the User schema and model
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+
+const User = mongoose.model('User', userSchema);
+app.get("/", (req, res) => {
+  res.json({ status: 'server is running' });
+})
+// API routes
+app.post('/signup', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // Check if the username is already taken
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(409).json({ error: 'Username already taken' });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user
+    const user = new User({ username, password: hashedPassword });
+    await user.save();
+
+    res.status(201).json({ message: 'User created successfully' });
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.post('/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // Find the user by username
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(401).json({ error: 'Invalid credentials' });
+    }
+
+    // Compare the provided password with the stored hashed password
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ error: 'Invalid credentials' });
+    }
+
+    // Generate a JWT token
+    const token = jwt.sign({ userId: user._id }, 'your_secret_key');
+
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
---
+++ text.js
@@ -0,0 +1,10 @@
+// {
+//     "_id": {
+//       "$oid": "649ec6a771af4121feacbd05"
+//     },
+//     "Name": "sanjana choubey",
+//     "occupation": "backend developer",
+//     "language": "nodejs, javascript",
+//     "experience": "four months"
+//   }
+  
